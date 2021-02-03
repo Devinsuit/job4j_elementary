ababaaa85406df5c3fc2fad938ea12606d2665d0
devinsuit <re.markbelfort@gmail.com>
2021-02-03 20:51:31 +0300

    9.2. Checkstyle [#465187]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         int one = 1;
         int two = 2;
         int six = 6;
-        int five =5;
+        int five = 5;
         int four = 4;
         int onePlusTwo = one + two;
         int sixDivTwo = six / two;
--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,14 +1,14 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k){
+    public static double square(int p, int k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
         double rsl = h * l;
         return rsl;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double result1 = SqArea.square(4, 1);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
 
